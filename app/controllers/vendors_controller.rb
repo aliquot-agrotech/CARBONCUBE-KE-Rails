3ac96af4b9f2ea168c8e9686edb44bb96673795a
LewKM <lewiskathembe51@gmail.com>
2024-06-30 10:26:01 +0300

    Adds the sign up code for the vendor and purchaser

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,19 +1,23 @@
 class VendorsController < ApplicationController
-    def create
-        vendor_params = vendor_params_with_categories
-        @vendor = Vendor.new(vendor_params.except(:category_ids))
-        @vendor.categories = Category.find(vendor_params[:category_ids])
-    
-        if @vendor.save
-            render json: @vendor, status: :created
-        else
-            render json: @vendor.errors, status: :unprocessable_entity
-        end
+    skip_before_action :verify_authenticity_token, only: [:signup]
+  
+    def signup
+      vendor_params_with_categories = vendor_params
+      @vendor = Vendor.new(vendor_params_with_categories.except(:category_ids))
+      @vendor.categories = Category.find(vendor_params_with_categories[:category_ids])
+      
+      if @vendor.save
+        token = JsonWebToken.encode(vendor_id: @vendor.id, role: 'vendor')
+        render json: { token: token, vendor: @vendor }, status: :created
+      else
+        render json: { errors: @vendor.errors.full_messages }, status: :unprocessable_entity
+      end
     end
   
     private
   
     def vendor_params
-        params.require(:vendor).permit(:fullname, :phone_number, :email, :enterprise_name, :location, :password, :password_confirmation, :business_registration_number, category_ids: [])
+      params.require(:vendor).permit(:fullname, :phone_number, :email, :enterprise_name, :location, :password, :password_confirmation, :business_registration_number, category_ids: [])
     end
   end
+  
