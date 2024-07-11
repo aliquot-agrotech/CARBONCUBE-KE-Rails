9f633a41a4cfb32156b2929468561f6754cdefd4
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-07-11 11:08:39 +0300

    Updates the vendor orders fetch

--- app/serializers/order_item_serializer.rb
+++ app/serializers/order_item_serializer.rb
@@ -1,10 +1,19 @@
+# app/serializers/order_item_serializer.rb
 class OrderItemSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :product_name
+  attributes :id, :quantity, :product_name, :price, :vendor_name
 
   belongs_to :order
   belongs_to :product
 
   def product_name
     object.product.title
+  end
+
+  def price
+    object.product.price
+  end
+
+  def vendor_name
+    object.product.vendor.fullname
   end
 end
