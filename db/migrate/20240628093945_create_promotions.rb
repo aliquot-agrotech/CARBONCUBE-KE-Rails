1216eed478f304d4c94f42c6533b35e61c657af1
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-08-12 10:01:27 +0300

    Updates the promotions model

--- db/migrate/20240628093945_create_promotions.rb
+++ db/migrate/20240628093945_create_promotions.rb
@@ -5,6 +5,7 @@ class CreatePromotions < ActiveRecord::Migration[7.1]
       t.string :title
       t.text :description
       t.decimal :discount_percentage
+      t.string :coupon_code
       t.datetime :start_date
       t.datetime :end_date
 
