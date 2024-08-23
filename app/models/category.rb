be3b790307d6255c281bdd6ef3da382b5ae49dc4
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-08-23 09:47:33 +0300

    Update the categories controller to ensure successful creation, updating and deletion of subcategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
     has_and_belongs_to_many :vendors
     has_many :subcategories, dependent: :destroy
     has_many :products, dependent: :nullify
+    accepts_nested_attributes_for :subcategories
 
     validates :name, presence: true
 end
