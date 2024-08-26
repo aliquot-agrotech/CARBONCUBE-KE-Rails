44ec936f0b84b0077a2fde9419a256b29c32e94e
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-08-26 11:51:48 +0300

    Update sthe vendor analytics controller for more information fetching

--- app/controllers/vendor/analytics_controller.rb
+++ app/controllers/vendor/analytics_controller.rb
@@ -2,13 +2,15 @@ class Vendor::AnalyticsController < ApplicationController
   before_action :authenticate_vendor
 
   def index
-    analytics_data = {
+    render json: {
       total_orders: calculate_total_orders,
       total_revenue: calculate_total_revenue,
+      total_products: calculate_total_products,
       average_rating: calculate_average_rating,
-      reviews: fetch_reviews
+      total_reviews: calculate_total_reviews,
+      sales_performance: calculate_sales_performance,
+      best_selling_products: fetch_best_selling_products
     }
-    render json: analytics_data
   end
 
   private
@@ -21,19 +23,41 @@ class Vendor::AnalyticsController < ApplicationController
     current_vendor.orders.joins(order_items: :product).sum('order_items.quantity * products.price')
   end
 
+  def calculate_total_products
+    current_vendor.products.count
+  end
+
   def calculate_average_rating
     current_vendor.products.joins(:reviews).average(:rating).to_f.round(1)
   end
 
-  def fetch_reviews
-    current_vendor.reviews.includes(:product).map do |review|
-      {
-        id: review.id,
-        product_name: review.product.title,
-        rating: review.rating,
-        comment: review.review
-      }
-    end
+  def calculate_total_reviews
+    current_vendor.reviews.count
+  end
+
+  def calculate_sales_performance
+    current_month = Date.current.beginning_of_month
+    three_months_ago = 3.months.ago.beginning_of_month
+  
+    sales_performance = current_vendor.orders.joins(:order_items)
+                             .where(created_at: three_months_ago..current_month.end_of_month)
+                             .group("DATE_TRUNC('month', orders.created_at)")
+                             .sum('order_items.quantity * order_items.price')
+                             .transform_keys { |k| k.strftime("%B %Y") }
+  
+    sales_performance
+  end
+  
+
+  def fetch_best_selling_products
+    best_selling_products = current_vendor.products.joins(:order_items)
+                                  .select('products.id AS product_id, products.title AS product_title, products.price AS product_price, SUM(order_items.quantity) AS total_sold')
+                                  .group('products.id')
+                                  .order('total_sold DESC')
+                                  .limit(3)
+                                  .map { |record| { product_id: record.product_id, product_title: record.product_title, product_price: record.product_price, total_sold: record.total_sold } }
+    
+    best_selling_products
   end
 
   def authenticate_vendor
