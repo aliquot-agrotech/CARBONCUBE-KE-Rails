a352ba5e7fd85e566d8905427c77f2035e47f5c6
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-09-13 11:47:53 +0300

    Upgrades the products serilaizer for category and sub category display

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,8 +1,7 @@
 # app/serializers/product_serializer.rb
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :vendor_id, :category_id, :subcategory_id, :title, :description, :price, :quantity, :brand, :manufacturer, :item_weight, :item_length, :item_width, :item_height, :media_urls, :first_media_url
+  attributes :id, :vendor_id, :category_id, :subcategory_id, :category_name, :subcategory_name, :title, :description, :price, :quantity, :brand, :manufacturer, :item_weight, :item_length, :item_width, :item_height, :media_urls, :first_media_url
 
-  
   has_one :vendor, serializer: VendorSerializer
   has_many :reviews
 
@@ -10,8 +9,17 @@ class ProductSerializer < ActiveModel::Serializer
     object.media
   end
 
-
   def vendor_name
     object.vendor.name
   end
+
+  # Add these methods to fetch the names
+  def category_name
+    object.category.name if object.category
+  end
+
+  def subcategory_name
+    object.subcategory.name if object.subcategory
+  end
 end
+  
