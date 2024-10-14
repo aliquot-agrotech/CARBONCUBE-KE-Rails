9f9c871ec6b1b0f9ae2753af443286dabeb6a828
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-10-14 21:05:07 +0300

    Updates the intializers for cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://carboncube-ke-react.vercel.app/"
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
