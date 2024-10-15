45bc9f7ce88cbcb7b758c6ec7597d4fa0eecd97e
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-10-15 11:23:11 +0300

    update sthe appname of my cors connfiguration

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CARBONCUBE-KE-Rails
+module CARBONCUBE_KE_Rails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
