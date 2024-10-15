70c00d2b3b86eb44fb35282ce5054f346ec06694
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-10-16 00:00:03 +0300

    Updates the new render-build.sh code

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,12 +8,6 @@ bundle config set frozen false
 # Install dependencies
 bundle install
 
-# Check if any tables exist
-if bundle exec rails db:exists; then
-  echo "Database already has tables, skipping migrations and seeds."
-else
-  # Run migrations and seeds
-  echo "Running migrations and seeds..."
-  bundle exec rake db:migrate
-  bundle exec rake db:seed
-fi
+# Run migrations and seeds
+bundle exec rake db:migrate
+bundle exec rake db:seed
