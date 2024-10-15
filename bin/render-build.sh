b47b019731c8d7044d89c52c3580a1aabdbc0423
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-10-15 09:58:59 +0300

    Updates the render-build.sh file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
-bundle install --with development test
+# Unfreeze to allow updating Gemfile.lock
+bundle config set frozen false
+
+# Install dependencies
+bundle install
+
+# Run migrations and seeds
 bundle exec rake db:migrate
 bundle exec rake db:seed
