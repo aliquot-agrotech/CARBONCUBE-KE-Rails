e96bbee41f44d61b53f89c2d2de104c1a8ec5002
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-13 16:16:49 +0300

    Manages to sort the db for the rider

---
+++ db/migrate/20241113131437_create_riders.rb
@@ -0,0 +1,20 @@
+class CreateRiders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :riders do |t|
+      t.string :full_name
+      t.string :phone_number
+      t.date :date_of_birth
+      t.string :email
+      t.string :id_number
+      t.string :driving_license
+      t.string :vehicle_type
+      t.string :license_plate
+      t.string :password_digest
+      t.string :next_of_kin_full_name
+      t.string :relationship
+      t.string :emergency_contact_phone_number
+
+      t.timestamps
+    end
+  end
+end
