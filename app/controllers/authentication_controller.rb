42e9c78e86ac409444f75e2d45e4161268f7512a
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-14 09:08:44 +0300

    Adds the new login for the rider

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,29 +1,28 @@
 class AuthenticationController < ApplicationController
-    skip_before_action :verify_authenticity_token, raise: false
-  
-    def login
-      @user = Purchaser.find_by(email: params[:email]) || Vendor.find_by(email: params[:email]) || Admin.find_by(email: params[:email])
-      
-      if @user&.authenticate(params[:password])
-        token = JsonWebToken.encode(user_id: @user.id, role: determine_role(@user))
-        render json: { token: token, user: { id: @user.id, email: @user.email, role: determine_role(@user) } }, status: :ok
-      else
-        render json: { errors: ['Invalid email or password'] }, status: :unauthorized
-      end
-    end
-  
-    private
-  
-    def determine_role(user)
-      if user.is_a?(Purchaser)
-        'purchaser'
-      elsif user.is_a?(Vendor)
-        'vendor'
-      elsif user.is_a?(Admin)
-        'admin'
-      else
-        'unknown'
-      end
+  skip_before_action :verify_authenticity_token, raise: false
+
+  def login
+    @user = Purchaser.find_by(email: params[:email]) || Vendor.find_by(email: params[:email]) || Admin.find_by(email: params[:email])
+    
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id, role: determine_role(@user))
+      render json: { token: token, user: { id: @user.id, email: @user.email, role: determine_role(@user) } }, status: :ok
+    else
+      render json: { errors: ['Invalid email or password'] }, status: :unauthorized
     end
   end
-  
+
+  private
+
+  def determine_role(user)
+    if user.is_a?(Purchaser)
+      'purchaser'
+    elsif user.is_a?(Vendor)
+      'vendor'
+    elsif user.is_a?(Admin)
+      'admin'
+    else
+      'unknown'
+    end
+  end
+end
