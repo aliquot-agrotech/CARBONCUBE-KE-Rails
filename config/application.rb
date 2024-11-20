0cb4aaca6a0aa566545ba3d20d9568afe976e4a4
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-20 12:43:54 +0300

    Updates the time_zone accodringlyfor the render server

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module CARBONCUBE_KE_Rails
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Africa/Nairobi"
+    config.time_zone = 'Africa/Nairobi' # Example for EAT time zone
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     # CORS configuration
