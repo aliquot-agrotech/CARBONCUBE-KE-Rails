85596422b9576b91f26a5ea6f72319f98cf478a2
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-20 12:27:46 +0300

    Changes the timezone to nairobi

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module CARBONCUBE_KE_Rails
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Africa/Nairobi"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # CORS configuration
