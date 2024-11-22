feba504edececad8015bd3003510cb8d231566cc
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-22 10:49:12 +0300

    Updates the model and controller for the rider

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -2,8 +2,11 @@ class Rider < ApplicationRecord
   has_secure_password
 
   # Validations
-  validates :full_name, :phone_number, :email, :id_number, :driving_license, :physical_address, :vehicle_type, :license_plate, :kin_full_name, :kin_relationship, :kin_phone_number, presence: true
+  validates :full_name, :phone_number, :email, :id_number, :gender, :driving_license, :physical_address, :vehicle_type, :license_plate, :kin_full_name, :kin_relationship, :kin_phone_number, presence: true
   validates :phone_number, uniqueness: true
   validates :kin_phone_number, uniqueness: true
   validates :email, uniqueness: true
+  validates :id_number, uniqueness: true
+  validates :driving_license, uniqueness: true
+  validates :license_plate, uniqueness: true
 end
