d344a92a5bc89e4b443d2b3b915ae76e79d8b11a
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-26 15:22:18 +0300

    Updates the blocked status for the vendor and purchaser

--- app/serializers/purchaser_serializer.rb
+++ app/serializers/purchaser_serializer.rb
@@ -1,4 +1,4 @@
 # app/serializers/purchaser_serializer.rb
 class PurchaserSerializer < ActiveModel::Serializer
-  attributes :id, :fullname, :email, :phone_number, :location, :username, :city, :zipcode, :profilepicture, :birthdate, :gender
+  attributes :id, :fullname, :email, :phone_number, :location, :username, :city, :zipcode, :profilepicture, :birthdate, :gender, :blocked
 end
--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -1,5 +1,5 @@
 class VendorSerializer < ActiveModel::Serializer
-  attributes :id, :fullname, :phone_number, :email, :enterprise_name, :location, :business_registration_number, :description, :username, :profilepicture, :birthdate, :zipcode, :city, :gender 
+  attributes :id, :fullname, :phone_number, :email, :enterprise_name, :location, :business_registration_number, :description, :username, :profilepicture, :birthdate, :zipcode, :city, :gender, :blocked 
 
   has_many :categories
 end
