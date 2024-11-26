fcff6dca0dc4ce609f30d3416d882531938b8c7d
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-26 15:30:48 +0300

    Updates the rider serilaizer to handle the status mode and value

--- app/serializers/rider_serializer.rb
+++ app/serializers/rider_serializer.rb
@@ -1,3 +1,3 @@
 class RiderSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :phone_number, :gender, :date_of_birth, :email, :id_number, :driving_license, :physical_address,:vehicle_type, :license_plate, :password, :kin_full_name, :kin_relationship, :kin_phone_number
+  attributes :id, :full_name, :phone_number, :gender, :date_of_birth, :email, :id_number, :blocked, :driving_license, :physical_address,:vehicle_type, :license_plate, :password, :kin_full_name, :kin_relationship, :kin_phone_number
 end
