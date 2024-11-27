1415c672c0875de66146cfaa7cd529186b9aeb27
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-11-27 10:25:27 +0300

    Tries to resolve the display of raw dat on the logs

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,92 +1,11 @@
-class Admin::OrdersController < ApplicationController
-  before_action :authenticate_admin
-  before_action :set_order, only: [:show, :update_status, :destroy]
-
-  # app/controllers/admin/orders_controller.rb
-  def index
-    if params[:search_query].present?
-      @orders = Order.joins(:purchaser)
-                    .where("vendors.phone_number = :query OR purchasers.phone_number = :query OR orders.id = :query", query: params[:search_query])
-                    .includes(:purchaser, order_items: { product: :vendor })
-    else
-      @orders = Order.includes(:purchaser, order_items: { product: :vendor }).all
-    end
-  
-    render json: @orders.as_json(
-      include: {
-        purchaser: { only: [:fullname] },
-        order_items: {
-          include: {
-            product: {
-              include: { vendor: { only: [:fullname] } }
-            }
-          }
-        }
-      },
-      methods: [:order_date, :total_price]
-    )
-  end
-  
-  def show
-    render json: @order.as_json(
-      include: {
-        purchaser: { only: [:fullname] },
-        order_items: {
-          include: {
-            product: {
-              include: { vendor: { only: [:fullname] } }
-            }
-          }
-        }
-      },
-      methods: [:order_date, :total_price]
-    )
+def index
+  if params[:search_query].present?
+    @orders = Order.joins(:purchaser)
+                  .where("vendors.phone_number = :query OR purchasers.phone_number = :query OR orders.id = :query", query: params[:search_query])
+                  .includes(:purchaser, order_items: { product: :vendor })
+  else
+    @orders = Order.includes(:purchaser, order_items: { product: :vendor }).all
   end
 
-  # PUT /admin/orders/:id/update-status
-  def update_status
-    if @order.update(status: params[:status])
-      render json: @order.as_json(
-        include: {
-          purchaser: { only: [:fullname] },
-          order_items: {
-            include: {
-              product: {
-                include: { vendor: { only: [:fullname] } }
-              }
-            }
-          }
-        },
-        methods: [:order_date, :total_price]
-      )
-    else
-      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @order.destroy
-    head :no_content
-  end
-
-  private
-
-  def set_order
-    @order = Order.find(params[:id])
-  end
-
-  def order_params
-    params.require(:order).permit(:purchaser_id, :status, :total_amount)
-  end
-  
-  def authenticate_admin
-    @current_user = AdminAuthorizeApiRequest.new(request.headers).result
-    unless @current_user && @current_user.is_a?(Admin)
-      render json: { error: 'Not Authorized' }, status: :unauthorized
-    end
-  end
-
-  def current_admin
-    @current_user
-  end
+  render json: @orders, each_serializer: OrderSerializer
 end
