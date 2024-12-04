95a09e416b233a85f9bc912932d2baf593c58aec
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-12-04 14:31:00 +0300

    Corrects the vendor full name error

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -10,67 +10,53 @@ class Admin::AnalyticsController < ApplicationController
 
     # Top 6 Best Selling Products Overall
     best_selling_products = Product.joins(:order_items)
-                                  .select('products.id AS product_id, products.title AS product_title, products.price AS product_price, SUM(order_items.quantity) AS total_sold, products.media AS media')
-                                  .group('products.id')
-                                  .order('total_sold DESC')
-                                  .limit(6)
-                                  .map { |record| 
-                                    {
-                                      product_id: record.product_id,
-                                      product_title: record.product_title,
-                                      product_price: record.product_price,
-                                      total_sold: record.total_sold,
-                                      media: record.media
-                                    }
-                                  }
+                            .select('products.id AS product_id, products.title AS product_title, products.price AS product_price, SUM(order_items.quantity) AS total_sold, products.media AS media')
+                            .group('products.id')
+                            .order('total_sold DESC')
+                            .limit(6)
+                            .map { |record| 
+                              {
+                                product_id: record.product_id,
+                                product_title: record.product_title,
+                                product_price: record.product_price,
+                                total_sold: record.total_sold,
+                                media: record.media
+                              }
+                            }
 
     # Total Products Sold Out
     total_products_sold_out = Product.joins(:order_items)
-                                    .distinct
-                                    .count
+                              .distinct
+                              .count
 
     # Top 10 Purchasers Insights
     purchasers_insights = Purchaser.joins(:orders)
-                                  .select('purchasers.fullname, COUNT(orders.id) AS total_orders')
-                                  .group('purchasers.id')
-                                  .order('total_orders DESC')
-                                  .limit(10)
+                          .select('purchasers.fullname, COUNT(orders.id) AS total_orders')
+                          .group('purchasers.id')
+                          .order('total_orders DESC')
+                          .limit(10)
 
     # Get selected metric from query parameter, default to 'Total Orders' if none provided
     selected_metric = params[:metric] || 'Total Orders'
 
     # Calculate total orders
     vendors_by_orders = Vendor.joins(orders: :order_items)
-                              .select(
-                                'vendors.id AS vendor_id',
-                                'vendors.fullname',
-                                'COUNT(DISTINCT orders.id) AS total_orders'
-                              )
-                              .group('vendors.id')
-                              .order('total_orders DESC')
+                        .select( 'vendors.id AS vendor_id, vendors.fullname, COUNT(DISTINCT orders.id) AS total_orders') 
+                        .group('vendors.id')
+                        .order('total_orders DESC')
 
-    # Calculate total revenue
-    # vendors_by_revenue = Vendor.joins(orders: :order_items)
-    #                           .joins(products: :order_items)
-    #                           .select(
-    #                             'vendors.id AS vendor_id',
-    #                             'vendors.fullname',
-    #                             'SUM(order_items.quantity * order_items.price) AS total_revenue'
-    #                           )
-    #                           .group('vendors.id')
-    #                           .order('total_revenue DESC')
 
     vendors_by_revenue = Vendor.joins(products: :order_items)
-    .select('vendors.id, vendors.fullname, SUM(order_items.total_price) AS total_revenue')
-    .group('vendors.id')
-    .order('total_revenue DESC')
+                        .select('vendors.id, vendors.fullname, SUM(order_items.total_price) AS total_revenue')
+                        .group('vendors.id')
+                        .order('total_revenue DESC')
 
 
     # Calculate mean rating
     vendors_by_rating = Vendor.joins(products: :reviews)
-    .select('vendors.id, vendors.name, AVG(reviews.rating) AS average_rating')
-    .group('vendors.id')
-    .order('average_rating DESC')
+                        .select('vendors.id, vendors.fullname, AVG(reviews.rating) AS average_rating')
+                        .group('vendors.id')
+                        .order('average_rating DESC')
 
     # Dynamically select the vendors' insights based on the metric
     case selected_metric
@@ -96,18 +82,18 @@ class Admin::AnalyticsController < ApplicationController
     three_months_ago = 2.months.ago.beginning_of_month
 
     sales_performance = Order.joins(:order_items)
-                            .where(created_at: three_months_ago..current_month.end_of_month)
-                            .group("DATE_TRUNC('month', orders.created_at)")
-                            .sum('order_items.price * order_items.quantity')
-                            .transform_keys { |k| k.strftime("%B %Y") }
+                        .where(created_at: three_months_ago..current_month.end_of_month)
+                        .group("DATE_TRUNC('month', orders.created_at)")
+                        .sum('order_items.price * order_items.quantity')
+                        .transform_keys { |k| k.strftime("%B %Y") }
 
     # Best Selling Categories Analytics
     best_selling_categories = Category.joins(products: :order_items)
-                                      .select('categories.name AS category_name, SUM(order_items.quantity) AS total_sold')
-                                      .group('categories.id')
-                                      .order('total_sold DESC')
-                                      .limit(4)
-                                      .map { |record| { category_name: record.category_name, total_sold: record.total_sold } }
+                              .select('categories.name AS category_name, SUM(order_items.quantity) AS total_sold')
+                              .group('categories.id')
+                              .order('total_sold DESC')
+                              .limit(4)
+                              .map { |record| { category_name: record.category_name, total_sold: record.total_sold } }
 
     render json: {
       total_vendors: @total_vendors,
