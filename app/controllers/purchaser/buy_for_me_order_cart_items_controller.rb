d3c63bf163460bb1e7494acfa4327dd6c4c022b3
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-12-10 14:28:11 +0300

    Upgrades the buy for me files

---
+++ app/controllers/purchaser/buy_for_me_order_cart_items_controller.rb
@@ -0,0 +1,65 @@
+# app/controllers/purchaser/buy_for_me_order_cart_items_controller.rb
+
+class Purchaser::BuyForMeOrderCartItemsController < ApplicationController
+  before_action :authenticate_purchaser
+
+  # GET /purchaser/buy_for_me_order_cart_items
+  def index
+    @buy_for_me_order_cart_items = current_purchaser.buy_for_me_order_cart_items.includes(:product)
+    total_cart_price = @buy_for_me_order_cart_items.sum(&:total_price)
+    render json: { buy_for_me_order_cart_items: @buy_for_me_order_cart_items, total_cart_price: total_cart_price }, include: ['product']
+  end
+
+  # POST /purchaser/buy_for_me_order_cart_items
+  def create
+    @buy_for_me_order_cart_items = current_purchaser.buy_for_me_order_cart_items.build(buy_for_me_order_cart_item_params)
+
+    if @buy_for_me_order_cart_items.save
+      update_cart_total_price
+      render json: @buy_for_me_order_cart_items, status: :created
+    else
+      render json: @buy_for_me_order_cart_items.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Purchaser::CartItemsController
+  def update
+    buy_for_me_order_cart_item = BuyForMeOrderCartItem.find(params[:id])
+    if buy_for_me_order_cart_item.update(quantity: params[:quantity])
+      render json: buy_for_me_order_cart_item, status: :ok
+    else
+      render json: { error: 'Failed to update item' }, status: :unprocessable_entity
+    end
+  end
+
+
+  # DELETE /purchaser/buy_for_me_order_cart_items/:id
+  def destroy
+    @buy_for_me_order_cart_items = current_purchaser.buy_for_me_order_cart_items.find(params[:id])
+    @buy_for_me_order_cart_items.destroy
+    update_buy_for_me_order_cart_total_price
+    head :no_content
+  end
+
+  private
+
+  def buy_for_me_order_cart_item_params
+    params.require(:buy_for_me_order_cart_item).permit(:product_id, :quantity)
+  end
+
+  def authenticate_purchaser
+    @current_user = PurchaserAuthorizeApiRequest.new(request.headers).result
+    unless @current_user && @current_user.is_a?(Purchaser)
+      render json: { error: 'Not Authorized' }, status: :unauthorized
+    end
+  end
+
+  def current_purchaser
+    @current_user
+  end
+
+  def update_cart_total_price
+    total_price = current_purchaser.buy_for_me_order_cart_items.sum(&:total_price)
+    current_purchaser.update(buy_for_me_order_cart_total_price: total_price)
+  end
+end
---
+++ db/migrate/20241210112628_create_buy_for_me_order_cart_items.rb
@@ -0,0 +1,13 @@
+# db/migrate/[timestamp]_create_cart_items.rb
+class CreateBuyForMeOrderCartItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :buy_for_me_order_cart_items do |t|
+      t.references :purchaser, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity, null: false, default: 1
+      t.decimal :price, precision: 10, scale: 2, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
