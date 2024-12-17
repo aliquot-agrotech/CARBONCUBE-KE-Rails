e35a5b255c13fa91bcc8128017aaf39b8e27b31f
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-12-17 14:44:20 +0300

    Adds new purchaser_id fetching method

--- app/controllers/product_searches_controller.rb
+++ app/controllers/product_searches_controller.rb
@@ -1,6 +1,11 @@
 class ProductSearchesController < ApplicationController
+  before_action :authenticate_purchaser, only: [:create] # Ensure purchaser authentication
+
   def create
     product_search = ProductSearch.new(product_search_params)
+
+    # Assign purchaser_id only if @current_user is present
+    product_search.purchaser_id = @current_user.id if @current_user
 
     if product_search.save
       render json: { message: 'Search logged successfully' }, status: :created
@@ -11,7 +16,14 @@ class ProductSearchesController < ApplicationController
 
   private
 
+  def authenticate_purchaser
+    @current_user = PurchaserAuthorizeApiRequest.new(request.headers).result
+    unless @current_user && @current_user.is_a?(Purchaser)
+      render json: { error: 'Not Authorized' }, status: :unauthorized
+    end
+  end
+
   def product_search_params
-    params.permit(:search_term, :purchaser_id)
+    params.permit(:search_term)
   end
 end
