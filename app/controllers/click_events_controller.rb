788060b8fd07e2d7e0292c299a62b9bbac15f397
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-12-17 13:17:23 +0300

    Adds the new logic to get the purchaser id

--- app/controllers/click_events_controller.rb
+++ app/controllers/click_events_controller.rb
@@ -1,6 +1,9 @@
 class ClickEventsController < ApplicationController
+  before_action :set_purchaser_from_token
+
   def create
     click_event = ClickEvent.new(click_event_params)
+    click_event.purchaser_id ||= @current_purchaser&.id # Assign purchaser_id if not already in params
 
     if click_event.save
       render json: { message: 'Click logged successfully' }, status: :created
@@ -11,6 +14,20 @@ class ClickEventsController < ApplicationController
 
   private
 
+  # Extracts purchaser_id from token if Authorization header exists
+  def set_purchaser_from_token
+    token = request.headers['Authorization']&.split(' ')&.last
+    return unless token
+
+    begin
+      decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      @current_purchaser = Purchaser.find_by(id: decoded['purchaser_id'])
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+      @current_purchaser = nil
+    end
+  end
+
+  # Strong parameters
   def click_event_params
     params.permit(:event_type, :product_id, :purchaser_id, metadata: {})
   end
