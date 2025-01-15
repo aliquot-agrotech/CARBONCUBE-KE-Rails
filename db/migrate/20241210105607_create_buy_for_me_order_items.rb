2b4549dd573cbef23522b33b035ea806bec485be
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-01-15 11:11:04 +0300

    Renames the product to add on buy for me to solve the error

--- db/migrate/20241210105607_create_buy_for_me_order_items.rb
+++ db/migrate/20241210105607_create_buy_for_me_order_items.rb
@@ -2,7 +2,7 @@ class CreateBuyForMeOrderItems < ActiveRecord::Migration[7.1]
   def change
     create_table :buy_for_me_order_items do |t|
       t.references :buy_for_me_order, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
+      t.references :ad, null: false, foreign_key: true
       t.integer :quantity, null: false, default: 1
       t.decimal :price, precision: 10, scale: 2, default: "0.0", null: false
       t.decimal :total_price, precision: 10, scale: 2, default: "0.0", null: false
