0babd692b7bcea8c3b54b11d01c8a49e6fb8f707
Lewis Mwendwa Kathembe <lewiskathembe51@gmail.com>
2025-01-27 12:07:14 +0300

    Omits the wishlist conversion rate as we have have eradicted the sales and purchases done

--- app/controllers/vendor/analytics_controller.rb
+++ app/controllers/vendor/analytics_controller.rb
@@ -65,41 +65,41 @@ class Vendor::AnalyticsController < ApplicationController
       end
   end  
 
-# Group clicks by income ranges
-def group_clicks_by_income
-  ClickEvent.joins(ad: {}, purchaser: :income)
-            .where(ads: { vendor_id: current_vendor.id })
-            .group("incomes.range", :event_type)
-            .count
-            .transform_keys { |k| { income_range: k[0], event_type: k[1] } }
-end
+  # Group clicks by income ranges
+  def group_clicks_by_income
+    ClickEvent.joins(ad: {}, purchaser: :income)
+              .where(ads: { vendor_id: current_vendor.id })
+              .group("incomes.range", :event_type)
+              .count
+              .transform_keys { |k| { income_range: k[0], event_type: k[1] } }
+  end
 
-# Group clicks by education levels
-def group_clicks_by_education
-  ClickEvent.joins(ad: {}, purchaser: :education)
-            .where(ads: { vendor_id: current_vendor.id })
-            .group("educations.level", :event_type)
-            .count
-            .transform_keys { |k| { education_level: k[0], event_type: k[1] } }
-end
+  # Group clicks by education levels
+  def group_clicks_by_education
+    ClickEvent.joins(ad: {}, purchaser: :education)
+              .where(ads: { vendor_id: current_vendor.id })
+              .group("educations.level", :event_type)
+              .count
+              .transform_keys { |k| { education_level: k[0], event_type: k[1] } }
+  end
 
-# Group clicks by employment statuses
-def group_clicks_by_employment
-  ClickEvent.joins(ad: {}, purchaser: :employment)
-            .where(ads: { vendor_id: current_vendor.id })
-            .group("employments.status", :event_type)
-            .count
-            .transform_keys { |k| { employment_status: k[0], event_type: k[1] } }
-end
+  # Group clicks by employment statuses
+  def group_clicks_by_employment
+    ClickEvent.joins(ad: {}, purchaser: :employment)
+              .where(ads: { vendor_id: current_vendor.id })
+              .group("employments.status", :event_type)
+              .count
+              .transform_keys { |k| { employment_status: k[0], event_type: k[1] } }
+  end
 
-# Group clicks by sectors
-def group_clicks_by_sector
-  ClickEvent.joins(ad: {}, purchaser: :sector)
-            .where(ads: { vendor_id: current_vendor.id })
-            .group("sectors.name", :event_type)
-            .count
-            .transform_keys { |k| { sector: k[0], event_type: k[1] } }
-end
+  # Group clicks by sectors
+  def group_clicks_by_sector
+    ClickEvent.joins(ad: {}, purchaser: :sector)
+              .where(ads: { vendor_id: current_vendor.id })
+              .group("sectors.name", :event_type)
+              .count
+              .transform_keys { |k| { sector: k[0], event_type: k[1] } }
+  end
 
   # Data for Free tier
   def calculate_free_tier_data
@@ -140,21 +140,19 @@ end
     }
   end
 
-
   # Wishlist Stats with Demographics
-def calculate_wishlist_stats
-  {
-    top_wishlisted_products: fetch_top_wishlisted_products,
-    wishlist_conversion_rate: calculate_wishlist_conversion_rate,
-    wishlist_trends: calculate_wishlist_trends,
-    wishlist_by_age_groups: group_wishlist_by_age,
-    wishlist_by_income_ranges: group_wishlist_by_income,
-    wishlist_by_education_levels: group_wishlist_by_education,
-    wishlist_by_employment_statuses: group_wishlist_by_employment,
-    wishlist_by_sectors: group_wishlist_by_sector
-  }
-end
-
+  def calculate_wishlist_stats
+    {
+      top_wishlisted_products: fetch_top_wishlisted_products,
+      # wishlist_conversion_rate: calculate_wishlist_conversion_rate,
+      wishlist_trends: calculate_wishlist_trends,
+      wishlist_by_age_groups: group_wishlist_by_age,
+      wishlist_by_income_ranges: group_wishlist_by_income,
+      wishlist_by_education_levels: group_wishlist_by_education,
+      wishlist_by_employment_statuses: group_wishlist_by_employment,
+      wishlist_by_sectors: group_wishlist_by_sector
+    }
+  end
 
 # def group_wishlist_by_age
 #   WishList.joins(:purchaser)
@@ -166,63 +164,53 @@ end
 #           end
 # end
 
-# Group wishlists by age groups
-def group_wishlist_by_age
-  WishList.joins(:ad)  # Ensure ads table is joined
-          .joins("INNER JOIN purchasers ON wish_lists.purchaser_id = purchasers.id")
-          .where(ads: { vendor_id: current_vendor.id })
-          .group("FLOOR(DATE_PART('year', AGE(purchasers.birthdate)) / 5) * 5")
-          .count
-          .transform_keys do |k|
-            { age_group: "#{k}–#{k.to_i + 4}" }
-          end
-end
+  # Group wishlists by age groups
+  def group_wishlist_by_age
+    WishList.joins(:ad)  # Ensure ads table is joined
+            .joins("INNER JOIN purchasers ON wish_lists.purchaser_id = purchasers.id")
+            .where(ads: { vendor_id: current_vendor.id })
+            .group("FLOOR(DATE_PART('year', AGE(purchasers.birthdate)) / 5) * 5")
+            .count
+            .transform_keys do |k|
+              { age_group: "#{k}–#{k.to_i + 4}" }
+            end
+  end
 
+  # Group wishlists by income ranges
+  def group_wishlist_by_income
+    WishList.joins(:ad, purchaser: :income)  # Using associations to join 'ad' and 'income' through 'purchaser'
+            .where(ads: { vendor_id: current_vendor.id })
+            .group("incomes.range")
+            .count
+            .transform_keys { |k| { income_range: k } }
+  end
 
-# Group wishlists by income ranges
-def group_wishlist_by_income
-  WishList.joins(:ad, purchaser: :income)  # Using associations to join 'ad' and 'income' through 'purchaser'
-          .where(ads: { vendor_id: current_vendor.id })
-          .group("incomes.range")
-          .count
-          .transform_keys { |k| { income_range: k } }
-end
+  # Group wishlists by education levels
+  def group_wishlist_by_education
+    WishList.joins(:ad, purchaser: :education)  # Using associations to join 'ad' and 'education' through 'purchaser'
+            .where(ads: { vendor_id: current_vendor.id })
+            .group("educations.level")
+            .count
+            .transform_keys { |k| { education_level: k } }
+  end
 
+  # Group wishlists by employment statuses
+  def group_wishlist_by_employment
+    WishList.joins(:ad, purchaser: :employment)  # Using associations to join 'ad' and 'employment' through 'purchaser'
+            .where(ads: { vendor_id: current_vendor.id })
+            .group("employments.status")
+            .count
+            .transform_keys { |k| { employment_status: k } }
+  end
 
-
-# Group wishlists by education levels
-def group_wishlist_by_education
-  WishList.joins(:ad, purchaser: :education)  # Using associations to join 'ad' and 'education' through 'purchaser'
-          .where(ads: { vendor_id: current_vendor.id })
-          .group("educations.level")
-          .count
-          .transform_keys { |k| { education_level: k } }
-end
-
-
-
-# Group wishlists by employment statuses
-def group_wishlist_by_employment
-  WishList.joins(:ad, purchaser: :employment)  # Using associations to join 'ad' and 'employment' through 'purchaser'
-          .where(ads: { vendor_id: current_vendor.id })
-          .group("employments.status")
-          .count
-          .transform_keys { |k| { employment_status: k } }
-end
-
-
-
-# Group wishlists by sectors
-def group_wishlist_by_sector
-  WishList.joins(:ad, purchaser: :sector)  # Using associations to join 'ad' and 'sector' through 'purchaser'
-          .where(ads: { vendor_id: current_vendor.id })
-          .group("sectors.name")
-          .count
-          .transform_keys { |k| { sector: k } }
-end
-
-
-
+  # Group wishlists by sectors
+  def group_wishlist_by_sector
+    WishList.joins(:ad, purchaser: :sector)  # Using associations to join 'ad' and 'sector' through 'purchaser'
+            .where(ads: { vendor_id: current_vendor.id })
+            .group("sectors.name")
+            .count
+            .transform_keys { |k| { sector: k } }
+  end
 
   def fetch_top_wishlisted_products
     WishList.joins(:ad)
@@ -232,59 +220,67 @@ end
             .order('wishlist_count DESC')
             .limit(3)
             .map { |record| { ad_title: record.ad_title, wishlist_count: record.wishlist_count } }
-  end    
+  end   
 
   # def calculate_wishlist_conversion_rate
-  #   current_vendor.ads
-  #                 .joins("LEFT JOIN wish_lists ON wish_lists.ad_id = ads.id")
-  #                 .joins("LEFT JOIN order_items ON order_items.ad_id = ads.id")
-  #                 .select('ads.title AS ad_title, COUNT(wish_lists.id) AS wishlist_count, SUM(order_items.quantity) AS purchase_count')
-  #                 .group('ads.title')  # Add 'ads.title' to the GROUP BY clause
-  #                 .map { |ad| { ad_title: ad.ad_title, wishlist_count: ad.wishlist_count, purchase_count: ad.purchase_count || 0 } }
-  # end
+  #   ads = current_vendor.ads
+  #                       .joins("LEFT JOIN wish_lists ON wish_lists.ad_id = ads.id")
+  #                       .joins("LEFT JOIN order_items ON order_items.ad_id = ads.id")
+  #                       .select('ads.title AS ad_title, 
+  #                                 COUNT(wish_lists.id) AS wishlist_count, 
+  #                                 SUM(order_items.quantity) AS purchase_count')
+  #                       .group('ads.title')
   
-  def calculate_wishlist_conversion_rate
-    ads = current_vendor.ads
-                        .joins("LEFT JOIN wish_lists ON wish_lists.ad_id = ads.id")
-                        .joins("LEFT JOIN order_items ON order_items.ad_id = ads.id")
-                        .select('ads.title AS ad_title, 
-                                  COUNT(wish_lists.id) AS wishlist_count, 
-                                  SUM(order_items.quantity) AS purchase_count')
-                        .group('ads.title')
+  #   # Calculate conversion rates and add fallback logic
+  #   processed_ads = ads.map do |ad|
+  #     wishlist_count = ad.wishlist_count.to_i
+  #     purchase_count = ad.purchase_count.to_i
+  #     conversion_rate = wishlist_count > 0 ? (purchase_count.to_f / wishlist_count) : 0
   
-    # Calculate conversion rates and add fallback logic
-    processed_ads = ads.map do |ad|
-      wishlist_count = ad.wishlist_count.to_i
-      purchase_count = ad.purchase_count.to_i
-      conversion_rate = wishlist_count > 0 ? (purchase_count.to_f / wishlist_count) : 0
+  #     {
+  #       ad_title: ad.ad_title,
+  #       wishlist_count: wishlist_count,
+  #       purchase_count: purchase_count,
+  #       conversion_rate: conversion_rate
+  #     }
+  #   end
   
-      {
-        ad_title: ad.ad_title,
-        wishlist_count: wishlist_count,
-        purchase_count: purchase_count,
-        conversion_rate: conversion_rate
-      }
-    end
+  #   # Sort by conversion rate or wishlist count as a fallback
+  #   sorted_ads =  if processed_ads.any? { |ad| ad[:purchase_count] > 0 }
+  #                   processed_ads.sort_by { |ad| -ad[:conversion_rate] }
+  #                 else
+  #                     processed_ads.sort_by { |ad| -ad[:wishlist_count] }
+  #                 end
   
-    # Sort by conversion rate or wishlist count as a fallback
-    sorted_ads = if processed_ads.any? { |ad| ad[:purchase_count] > 0 }
-                   processed_ads.sort_by { |ad| -ad[:conversion_rate] }
-                 else
-                   processed_ads.sort_by { |ad| -ad[:wishlist_count] }
-                 end
-  
-    # Ensure it always returns an array
-    sorted_ads.first(3) || []
-  end  
+  #   # Ensure it always returns an array
+  #   sorted_ads.first(3) || []
+  # end  
   
 
   def calculate_wishlist_trends
-    WishList.joins(:ad)
-            .where(ads: { vendor_id: current_vendor.id })
-            .group('DATE_TRUNC(\'month\', wish_lists.created_at)', 'ads.title')  # Ensure both fields are grouped
-            .count
-            .transform_keys { |k| { month: k[0].strftime("%B %Y"), ad_title: k[1] } }
-  end  
+    # Get the date range for the last 5 months
+    end_date = Date.today.beginning_of_month
+    start_date = end_date - 5.months
+  
+    # Query the wishlists for the vendor within the date range, grouped by month
+    wishlist_counts = WishList.joins(:ad)
+                              .where(ads: { vendor_id: current_vendor.id })
+                              .where('wish_lists.created_at >= ? AND wish_lists.created_at <= ?', start_date, end_date)
+                              .group('DATE_TRUNC(\'month\', wish_lists.created_at)')
+                              .count
+  
+    # Prepare the result in the desired format (an array of hashes with month names and counts)
+    monthly_wishlist_counts = []
+    (start_date..end_date).to_a.reverse.each do |month_date|
+      month_key = month_date.beginning_of_month.to_date.strftime('%Y-%m')
+      wishlist_count = wishlist_counts[month_key] || 0
+      monthly_wishlist_counts << { month: month_date.strftime('%B %Y'), wishlist_count: wishlist_count }
+    end
+  
+    # Return the prepared data for the graph
+    monthly_wishlist_counts
+  end
+  
 
   # Competitor Stats
   def calculate_competitor_stats
