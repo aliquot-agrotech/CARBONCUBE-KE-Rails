262627997c69ab8392d19a1a93a2a80f9ad2f2c4
LewKM <lewiskathembe51@gmail.com>
2025-02-11 12:38:18 +0300

    Chnages the vdeor tier update routing and logic

--- app/controllers/vendor/vendor_tiers_controller.rb
+++ app/controllers/vendor/vendor_tiers_controller.rb
@@ -35,4 +35,35 @@ class Vendor::VendorTiersController < ApplicationController
       render json: { error: "Vendor tier not found." }, status: :not_found
     end
   end
+
+  def update_tier
+    tier = Tier.find_by(id: params[:tier_id]) # Find the tier by the provided ID
+
+    unless tier
+      return render json: { error: 'Invalid tier selected' }, status: :not_found
+    end
+
+    # Extract numeric duration from the string (e.g., "6 months" => 6)
+    tier_duration = params[:tier_duration].to_i
+
+    # Check if the vendor already has an entry in the vendor_tiers table
+    vendor_tier = VendorTier.where(vendor_id: @current_vendor.id, tier_id: tier.id).order(updated_at: :desc).first
+
+    if vendor_tier
+      # Update the existing entry
+      if vendor_tier.update(duration_months: tier_duration)
+        render json: vendor_tier, serializer: VendorTierSerializer, status: :ok
+      else
+        render json: { error: 'Tier update failed', details: vendor_tier.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      # Create a new entry in the vendor_tiers table
+      new_vendor_tier = VendorTier.new(vendor_id: @current_vendor.id, tier_id: tier.id, duration_months: tier_duration)
+      if new_vendor_tier.save
+        render json: new_vendor_tier, serializer: VendorTierSerializer, status: :created
+      else
+        render json: { error: 'Failed to create new tier', details: new_vendor_tier.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
 end
