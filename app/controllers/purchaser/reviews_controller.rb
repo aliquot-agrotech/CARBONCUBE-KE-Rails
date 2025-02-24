a059d2792cfdfb66b270237cc5c3dd3147ced2a7
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-02-24 11:32:12 +0300

    Updates the reviews controller

--- app/controllers/purchaser/reviews_controller.rb
+++ app/controllers/purchaser/reviews_controller.rb
@@ -1,21 +1,23 @@
 class Purchaser::ReviewsController < ApplicationController
   before_action :authenticate_purchaser
+  before_action :set_ad, only: [:index, :create]
   before_action :set_review, only: [:show, :update, :destroy]
 
-  # GET /purchaser/reviews
+  # GET /purchaser/ads/:ad_id/reviews
   def index
-    @reviews = current_purchaser.reviews
+    @reviews = @ad.reviews
     render json: @reviews
   end
 
-  # GET /purchaser/reviews/:id
+  # GET /purchaser/ads/:ad_id/reviews/:id
   def show
     render json: @review
   end
 
-  # POST /purchaser/reviews
+  # POST /purchaser/ads/:ad_id/reviews
   def create
-    @review = current_purchaser.reviews.new(review_params)
+    @review = @ad.reviews.new(review_params)
+    @review.purchaser = current_purchaser
 
     if @review.save
       render json: @review, status: :created
@@ -24,7 +26,7 @@ class Purchaser::ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /purchaser/reviews/:id
+  # PATCH/PUT /purchaser/ads/:ad_id/reviews/:id
   def update
     if @review.update(review_params)
       render json: @review
@@ -33,7 +35,7 @@ class Purchaser::ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /purchaser/reviews/:id
+  # DELETE /purchaser/ads/:ad_id/reviews/:id
   def destroy
     @review.destroy
     head :no_content
@@ -41,19 +43,23 @@ class Purchaser::ReviewsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_review
-    @review = current_purchaser.reviews.find(params[:id])
+  def set_ad
+    @ad = Ad.find(params[:ad_id])
   end
 
-  # Only allow a list of trusted parameters through.
+  def set_review
+    @review = current_purchaser.reviews.find_by!(id: params[:id], ad_id: params[:ad_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Review not found' }, status: :not_found
+  end
+
   def review_params
-    params.require(:review).permit(:ad_id, :rating, :review)
+    params.require(:review).permit(:rating, :review)
   end
 
   def authenticate_purchaser
     @current_user = PurchaserAuthorizeApiRequest.new(request.headers).result
-    unless @current_user && @current_user.is_a?(Purchaser)
+    unless @current_user&.is_a?(Purchaser)
       render json: { error: 'Not Authorized' }, status: :unauthorized
     end
   end
@@ -62,4 +68,3 @@ class Purchaser::ReviewsController < ApplicationController
     @current_user
   end
 end
-  
