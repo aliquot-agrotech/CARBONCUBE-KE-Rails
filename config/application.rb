54727a33c6271493d6785cb6aad766e3dfa4df0a
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-04-14 10:43:26 +0300

    updates the application.rb file code configuration

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module CARBONCUBE_KE_Rails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Add this line:
+    config.middleware.use Rack::ContentLength
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
