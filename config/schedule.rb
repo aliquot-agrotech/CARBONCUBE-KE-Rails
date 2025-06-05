58d598a9df32f4bb5154d009c92c11a57e392921
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-06-05 08:41:07 +0300

    Checks for the updates info regarding the expiry dates of the documents

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,8 @@
-set :output, "log/cron.log"
+# config/schedule.rb
+
+set :output, "/root/CARBON/log/cron.log"  # Or change to a path inside your Rails backend container, if preferred
 env :PATH, ENV['PATH']
 
 every 1.day, at: '12:00 am' do
-  rake "documents:send_reminders"
+  command "cd /root/CARBON && docker-compose exec -T backend rake documents:send_reminders"
 end
