6b0e7ec557dd58eeade3ac062374347c8fd83ed7
Kimani <karaba.dev@gmail.com>
2025-08-05 16:28:31 +0300

    added logic to sales dashboard

--- app/controllers/sales/analytics_controller.rb
+++ app/controllers/sales/analytics_controller.rb
@@ -10,9 +10,42 @@ class Sales::AnalyticsController < ApplicationController
     total_ads_wish_listed = WishList.count
 
 
-    paid_seller_tiers_count = SellerTier.where(tier_id: [2, 3, 4]).count
+    paid_seller_tiers_count = SellerTier
+    .joins(:seller)
+    .where(tier_id: [2, 3, 4], sellers: { deleted: false })
+    .count
 
-    unpaid_seller_tiers = SellerTier.where(tier_id: 1).count
+    unpaid_seller_tiers = SellerTier
+    .joins(:seller)
+    .where(tier_id: 1, sellers: { deleted: false })
+    .count
+
+    total_ads_clicks = ClickEvent.where(event_type: 'Ad-Click').count
+
+    buyer_ad_clicks = ClickEvent
+    .joins(:buyer)  # assuming ClickEvent belongs_to :buyer
+    .where(event_type: 'Ad-Click')
+    .where(buyers: { deleted: false })
+    .count
+
+
+     category_click_events = Category.joins(ads: :click_events)
+        .where.not(click_events: { buyer_id: nil })
+        .select('categories.name AS category_name, 
+                SUM(CASE WHEN click_events.event_type = \'Ad-Click\' THEN 1 ELSE 0 END) AS ad_clicks,
+                SUM(CASE WHEN click_events.event_type = \'Add-to-Wish-List\' THEN 1 ELSE 0 END) AS wish_list_clicks,
+                SUM(CASE WHEN click_events.event_type = \'Reveal-Seller-Details\' THEN 1 ELSE 0 END) AS reveal_clicks')
+        .group('categories.id')
+        .order('category_name')
+        .map do |record| 
+          {
+            category_name: record.category_name,
+            ad_clicks: record.ad_clicks,
+            wish_list_clicks: record.wish_list_clicks,
+            reveal_clicks: record.reveal_clicks
+          }
+        end
+
 
     
 
@@ -23,7 +56,10 @@ class Sales::AnalyticsController < ApplicationController
       total_reviews: @total_reviews,
       total_ads_wish_listed: total_ads_wish_listed ,
       subscription_countdowns:paid_seller_tiers_count,
-      without_subscription: unpaid_seller_tiers
+      without_subscription: unpaid_seller_tiers,
+      total_ads_clicks: total_ads_clicks,
+      category_click_events: category_click_events,
+      buyer_ad_clicks: buyer_ad_clicks,
     }
   end
 
